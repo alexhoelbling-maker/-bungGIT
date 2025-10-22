0829eeeff2a777532a09867e0f4b5fb6edda4eb7
alex <alexhoelbling@icloud.com>
2025-10-22 12:57:56 +0200

    zweiter commit UE3

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    System.out.println("Hello and welcome!");
     //ich habe ein awway generiert und die for schleife auf eine for each geändert
     int [] arr={1,2,3,4,5,6,7,8,9,10};
     for (int s : arr) {
