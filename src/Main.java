30d7e0005fdfafca9e2a8a396fdf0e6bd992d083
alex <alexhoelbling@icloud.com>
2025-10-22 12:55:39 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,14 @@ void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
+    //ich habe ein awway generiert und die for schleife auf eine for each geändert
+    int [] arr={1,2,3,4,5,6,7,8,9,10};
+    for (int s : arr) {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+
+        //hi oliver
+        System.out.println("Hi Oliver");
+        IO.println("i = " + s);
     }
+
 }
