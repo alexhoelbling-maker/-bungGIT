aa65987dbcc3b0cc12ce68e9db2065fda14d0216
alex <alexhoelbling@icloud.com>
2025-10-22 13:15:43 +0200

    test commit UE4

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,17 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
+    System.out.println("Hi Oliver");
+    //ich will nach Hause
+    //hilfe
     for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+
         IO.println("i = " + i);
+    }
+    int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    for(int s : arr) {
+        System.out.println("Hi Oliver");
+        IO.println("i = " + s);
     }
 }
